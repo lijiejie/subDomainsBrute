b3071df3d7c1f545013e22cb6a18a3c281f00c83
lijiejie <my@lijiejie.com>
2015-12-21 22:39:52 +0800

    code optimized: reconstruct and new featurs

--- README.md
+++ README.md
@@ -1,31 +1,41 @@
 # subDomainsBrute
 
-A simple and fast sub domain brute tool for pentesters
+A simple and fast sub domain brute tool for pentesters.
 
 这个脚本的主要目标是发现其他工具无法探测到的域名. 比如大家常用的Google，aizhan，fofa。
 
+##Change Log
+* 字典统一到dict文件夹下
+* 精简二级域名字典，丰富三四域名字典
+* 增加-i参数，忽略指向内网IP域名
+* 默认由10线程调整为30线程，但增加了超时重试
+
 ##Dependencies
-First you need install [dnspython](http://www.dnspython.org/kits/1.12.0/) to do DNS query
+First you need to install [dnspython](http://www.dnspython.org/kits/1.12.0/) to do DNS query
+> pip install dnspython
 
 ## Improvements
 * 用小字典递归地发现三级域名，四级域名、五级域名等不容易被探测到的域名
-* 字典较为全面，小字典就包括3万多条，大字典多达8万条
-* 默认使用114DNS、百度DNS、阿里DNS这几个快速又可靠的公共DNS进行查询，可随时修改配置文件添加你认为可靠的DNS服务器
-* 自动筛选泛解析的域名，当前规则是： 超过10个域名指向同一IP，则此后发现的其他指向该IP的域名将被丢弃
-* 整体速度还过得去，在我的PC上，每秒稳定扫描100到200个域名（10个线程）
+* 字典较为丰富，小字典就包括1万5千条，大字典多达6万3千条
+* 默认使用114DNS、百度DNS、阿里DNS这几个快速又可靠的Public DNS查询，可修改配置文件添加DNS服务器
+* 自动去重泛解析的域名，当前规则： 超过2个域名指向同一IP，则此后发现的其他指向该IP的域名将被丢弃
+* 速度尚可，在我的PC上，每秒稳定扫描几百个域名（30个线程）
 
 ##Usage
 ```
-Usage: subDomainsBrute.py [options] target
+Usage: subDomainsBrute.py [options] target.com
 
 Options:
   -h, --help            show this help message and exit
   -t THREADS_NUM, --threads=THREADS_NUM
-                        Number of threads. default = 10
+                        Number of threads. default = 30
   -f NAMES_FILE, --file=NAMES_FILE
                         Dict file used to brute sub names
+  -i, --ignore-intranet
+                        Ignore domains pointed to private IPs.
   -o OUTPUT, --output=OUTPUT
                         Output file name. default is {target}.txt
+
 ```
 
 Output file could be like: [http://www.lijiejie.com/wp-content/uploads/2015/04/baidu.com_.txt](http://www.lijiejie.com/wp-content/uploads/2015/04/baidu.com_.txt)
--- subDomainsBrute.py
+++ subDomainsBrute.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-# A simple and fast sub domain brute tool
+# A simple and fast sub domains brute tool for pentesters
 # my[at]lijiejie.com (http://www.lijiejie.com)
 
-import optparse
 import Queue
 import sys
 import dns.resolver
 import threading
 import time
 import optparse
+import os
 from lib.consle_width import getTerminalSize
 
 
 class DNSBrute:
-    def __init__(self, target, names_file, threads_num, output):
+    def __init__(self, target, names_file, ignore_intranet, threads_num, output):
         self.target = target.strip()
         self.names_file = names_file
+        self.ignore_intranet = ignore_intranet
         self.thread_count = self.threads_num = threads_num
         self.scan_count = self.found_count = 0
         self.lock = threading.Lock()
-        self.console_width = getTerminalSize()[0]
-        self.console_width -= 2    # Cal width when starts up
+        self.console_width = getTerminalSize()[0] - 2    # Cal terminal width when starts up
         self.resolvers = [dns.resolver.Resolver() for _ in range(threads_num)]
         self._load_dns_servers()
         self._load_sub_names()
@@ -29,10 +29,11 @@ class DNSBrute:
         outfile = target + '.txt' if not output else output
         self.outfile = open(outfile, 'w')   # won't close manually
         self.ip_dict = {}
+        self.STOP_ME = False
 
     def _load_dns_servers(self):
         dns_servers = []
-        with open('dns_servers.txt') as f:
+        with open('dict/dns_servers.txt') as f:
             for line in f:
                 server = line.strip()
                 if server.count('.') == 3 and server not in dns_servers:
@@ -42,14 +43,15 @@ class DNSBrute:
 
     def _load_sub_names(self):
         self.queue = Queue.Queue()
-        with open(self.names_file) as f:
+        file = 'dict/' + self.names_file if not os.path.exists(self.names_file) else self.names_file
+        with open(file) as f:
             for line in f:
                 sub = line.strip()
                 if sub: self.queue.put(sub)
 
     def _load_next_sub(self):
         next_subs = []
-        with open('next_sub.txt') as f:
+        with open('dict/next_sub.txt') as f:
             for line in f:
                 sub = line.strip()
                 if sub and sub not in next_subs:
@@ -69,42 +71,60 @@ class DNSBrute:
         sys.stdout.flush()
         self.lock.release()
 
+    @staticmethod
+    def is_intranet(ip):
+        ret = ip.split('.')
+        if not len(ret) == 4:
+            return True
+        if ret[0] == '10':
+            return True
+        if ret[0] == '172' and 16 <= int(ret[1]) <= 32:
+            return True
+        if ret[0] == '192' and ret[1] == '168':
+            return True
+        return False
+
     def _scan(self):
         thread_id = int( threading.currentThread().getName() )
         self.resolvers[thread_id].nameservers = [self.dns_servers[thread_id % self.dns_count]]    # must be a list object
-        self.resolvers[thread_id].lifetime = self.resolvers[thread_id].timeout = 1.0
-        while self.queue.qsize() > 0 and self.found_count < 3000:    # limit found count to 3000
+        self.resolvers[thread_id].lifetime = self.resolvers[thread_id].timeout = 10.0
+        while self.queue.qsize() > 0 and not self.STOP_ME and self.found_count < 4000:    # limit found count to 4000
             sub = self.queue.get(timeout=1.0)
-            try:
-                cur_sub_domain = sub + '.' + self.target
-                answers = d.resolvers[thread_id].query(cur_sub_domain)
-                is_wildcard_record = False
-                if answers:
-                    for answer in answers:
-                        self.lock.acquire()
-                        if answer.address not in self.ip_dict:
-                            self.ip_dict[answer.address] = 1
-                        else:
-                            self.ip_dict[answer.address] += 1
-                            if self.ip_dict[answer.address] > 6:    # a wildcard DNS record
-                                is_wildcard_record = True
-                        self.lock.release()
-                    if is_wildcard_record:
-                        self._update_scan_count()
-                        self._print_progress()
-                        continue
-                    self.lock.acquire()
-                    self.found_count += 1
-                    ips = ', '.join([answer.address for answer in answers])
-                    msg = cur_sub_domain.ljust(30) + ips
-                    sys.stdout.write('\r' + msg + ' ' * (self.console_width- len(msg)) + '\n\r')
-                    sys.stdout.flush()
-                    self.outfile.write(cur_sub_domain.ljust(30) + '\t' + ips + '\n')
-                    self.lock.release()
-                    for i in self.next_subs:
-                        self.queue.put(i + '.' + sub)
-            except Exception, e:
-                pass
+            for _ in range(6):
+                try:
+                    cur_sub_domain = sub + '.' + self.target
+                    answers = d.resolvers[thread_id].query(cur_sub_domain)
+                    is_wildcard_record = False
+                    if answers:
+                        for answer in answers:
+                            self.lock.acquire()
+                            if answer.address not in self.ip_dict:
+                                self.ip_dict[answer.address] = 1
+                            else:
+                                self.ip_dict[answer.address] += 1
+                                if self.ip_dict[answer.address] > 2:    # a wildcard DNS record
+                                    is_wildcard_record = True
+                            self.lock.release()
+                        if is_wildcard_record:
+                            self._update_scan_count()
+                            self._print_progress()
+                            continue
+                        ips = ', '.join([answer.address for answer in answers])
+                        if (not self.ignore_intranet) or (not DNSBrute.is_intranet(answers[0].address)):
+                            self.lock.acquire()
+                            self.found_count += 1
+                            msg = cur_sub_domain.ljust(30) + ips
+                            sys.stdout.write('\r' + msg + ' ' * (self.console_width- len(msg)) + '\n\r')
+                            sys.stdout.flush()
+                            self.outfile.write(cur_sub_domain.ljust(30) + '\t' + ips + '\n')
+                            self.lock.release()
+                            for i in self.next_subs:
+                                self.queue.put(i + '.' + sub)
+                        break
+                except dns.resolver.NoNameservers, e:
+                    break
+                except Exception, e:
+                    pass
             self._update_scan_count()
             self._print_progress()
         self._print_progress()
@@ -118,17 +138,24 @@ class DNSBrute:
             t = threading.Thread(target=self._scan, name=str(i))
             t.setDaemon(True)
             t.start()
-        while self.thread_count > 0:
-            time.sleep(0.01)
-
+        while self.thread_count > 1:
+            try:
+                time.sleep(1.0)
+            except KeyboardInterrupt,e:
+                msg = '[WARNING] User aborted, wait all slave threads to exit...'
+                sys.stdout.write('\r' + msg + ' ' * (self.console_width- len(msg)) + '\n\r')
+                sys.stdout.flush()
+                self.STOP_ME = True
 
 if __name__ == '__main__':
-    parser = optparse.OptionParser('usage: %prog [options] target')
+    parser = optparse.OptionParser('usage: %prog [options] target.com')
     parser.add_option('-t', '--threads', dest='threads_num',
-              default=10, type='int',
-              help='Number of threads. default = 10')
-    parser.add_option('-f', '--file', dest='names_file', default='subnames.txt',
+              default=30, type='int',
+              help='Number of threads. default = 30')
+    parser.add_option('-f', '--file', dest='names_file', default='dict/subnames.txt',
               type='string', help='Dict file used to brute sub names')
+    parser.add_option('-i', '--ignore-intranet', dest='i', default=False, action='store_true',
+              help='Ignore domains pointed to private IPs.')
     parser.add_option('-o', '--output', dest='output', default=None,
               type='string', help='Output file name. default is {target}.txt')
 
@@ -138,8 +165,7 @@ if __name__ == '__main__':
         sys.exit(0)
 
     d = DNSBrute(target=args[0], names_file=options.names_file,
+                 ignore_intranet=options.i,
                  threads_num=options.threads_num,
                  output=options.output)
     d.run()
-
-
